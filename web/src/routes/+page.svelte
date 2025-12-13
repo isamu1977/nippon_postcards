2efe1751355a3cf557af614a742b5880665b79c1
isamu1977 <isamu.matsuyama77@gmail.com>
2025-12-13 16:17:33 +0900

    refactor: replace static content with HeroSection component

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
 	import { t } from "$lib/translations/translations";
+	import HeroSection from "$lib/components/landing/HeroSection.svelte";
 </script>
 
-<section>
-	<h1>SvelteKit app</h1>
-
-	<!-- ここで言語に応じた文言を表示してみる -->
-	<p>{$t("common.heroTitle")}</p>
-	<div class="h-[200px] w-[200px] bg-red-600">oi</div>
-</section>
+<HeroSection />
